1bb63286bf082021f74de1a778b3d4b8864084d0
lkribeirof <116752823+lkribeirof@users.noreply.github.com>
2023-12-17 23:16:24 -0300

    atualização do css do react
    atualização do react e do css

---
+++ seu_filme/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+import {BiCameraMovie, BiSearchAlt2} from "react-icons/bi";
+
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav id="navbar">
+        <h2>
+          <Link to="/"><BiCameraMovie />SeuFilme</Link>
+        </h2>
+        <form>
+            <input type="text" placeholder="Busque um filme" />
+            <button type="submit">
+                <BiSearchAlt2 />
+            </button>
+        </form>
+      </nav>
+    );
+};
+
+export default Navbar;
